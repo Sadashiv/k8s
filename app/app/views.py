b9504655d46009b29ac0ca0ecf508f7d927e0587
Sadashiv H <sadamsrit@gmail.com>
2022-11-06 16:35:15 +0530

    Added storage and configuration

--- app/app/views.py
+++ app/app/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.shortcuts import redirect
+import os
 
 # Create your views here.
 def index(request):
     """ """
+    get_color = os.environ.get('APP_COLOR')
     #return HttpResponse("Welcome to Sample Application home page 1.0")
     text = """
-            <body style="background-color:green;">
-            <h1 align="center" style="text-color:green;">Welcome to Djano Application home page 3.0</h1>
+            <body style="background-color:%s;">
+            <h1 align="center" style="text-color:%s;">Welcome to Djano Application with APP_COLOR</h1>
             </body>
-            """
+            """ %(get_color,get_color)
     return HttpResponse(text)
 
--- todo.md
+++ todo.md
@@ -3,5 +3,13 @@ List of activites to do
 
 1. Create private registry and push docker images
 
-create k8 service and access application
+deployment, create k8 service and access application
 differance beteween deployment and rs, rc
+ingress and egress
+DNS and networking
+upgrade k8s
+json and out-columns
+
+pv and pvc
+taint and tolerances, nodeaffinity
+configmap
